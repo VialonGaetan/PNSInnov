7f01d7bc7679eb928ec7bce377e8a06e46a91587
Gaetan Vialon <gaetan.vialon@etu.unice.fr>
2017-05-02 13:48:02 +0200

    Add RespondingCode

---
+++ src/main/java/fr/unice/polytech/transmission/answers/RespondingCode.java
@@ -0,0 +1,18 @@
+package fr.unice.polytech.transmission.answers;
+
+/**
+ * @author Gaetan Vialon
+ *         Created the 02/05/2017.
+ */
+public class RespondingCode {
+
+    private final int code;
+
+    public RespondingCode(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
